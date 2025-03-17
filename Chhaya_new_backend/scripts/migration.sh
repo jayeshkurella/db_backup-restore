5259c9de73e91d41f934e3a292be48907d9cfb9b
jayeshkurella <jayeshkurella3112@gmail.com>
2025-03-17 06:24:51 +0000

    Add new file

---
+++ Chhaya_new_backend/scripts/migration.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+project_directory="/var/lib/jenkins/workspace/chhaya_jenkins"
+
+echo "Changing directory to Python project directory"
+cd $project_directory
+if [ $? -ne 0 ]; then
+  echo "Failed to change directory to $project_directory"
+  exit 1
+fi
+
+echo "Activating the virtual environment"
+source venv/bin/activate
+
+echo "Checking if virtual environment is active after activation"
+if [ -z "$VIRTUAL_ENV" ]; then
+  echo "Failed to activate virtual environment."
+  exit 1
+else
+  echo "Virtual environment is active: $VIRTUAL_ENV"
+fi
+
+# Check if manage.py exists
+if [ ! -f "$project_directory/Chhaya_new_backend/manage.py" ]; then
+  echo "manage.py not found in $project_directory/flames"
+  exit 1
+fi
+
+echo "Processing for makemigrations"
+cd $project_directory/Chhaya_new_backend
+python3 manage.py makemigrations --noinput
+
+echo "Processing for migrations"
+python3 manage.py migrate
+
+echo "Migrations Done"
+
+echo "Deactivating the virtual environment"
+deactivate
+
+echo "Checking if virtual environment is active after deactivation"
+if [ -z "$VIRTUAL_ENV" ]; then
+  echo "No virtual environment is active."
+else
+  echo "Virtual environment is still active: $VIRTUAL_ENV"
+fi
