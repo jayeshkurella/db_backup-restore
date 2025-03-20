ee29d0fc7564d4b6cf45254754f3d12053cf2cb0
jayeshkurella <jayeshkurella3112@gmail.com>
2025-03-20 09:38:15 +0000

    Edit runserver.sh

--- Chhaya_new_backend/scripts/runserver.sh
+++ Chhaya_new_backend/scripts/runserver.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Activate virtual environment
 echo "Activating virtual environment..."
-source /var/lib/jenkins/workspace/chhaya_new/Chhaya_new_backend/venv/bin/activate
+source /var/lib/jenkins/workspace/chhaya_new/venv/bin/activate
 
-# Check if the virtual environment was activated successfully
-if [ $? -eq 0 ]; then
-  echo "Virtual environment activated successfully."
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "Virtual environment activated."
 else
-  echo "Failed to activate the virtual environment. Exiting..."
-  exit 1
+    echo "Failed to activate the virtual environment. Exiting..."
+    exit 1
 fi
 
-# Change to the project directory (optional, but it's a good practice)
+echo "Starting Django server..."
 cd /var/lib/jenkins/workspace/chhaya_new/Chhaya_new_backend
-
-# Run Django development server 
-echo "Starting the Django development server..."
-python manage.py runserver 0.0.0.0:8090
+python manage.py runserver 0.0.0.0:8000
