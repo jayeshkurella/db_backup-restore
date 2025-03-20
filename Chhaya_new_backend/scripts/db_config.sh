8959d4a2271fb583fe12a2ae50803d9579962b0d
jayeshkurella <jayeshkurella3112@gmail.com>
2025-03-20 09:20:04 +0000

    Edit db_config.sh

--- Chhaya_new_backend/scripts/db_config.sh
+++ Chhaya_new_backend/scripts/db_config.sh
@@ -4,19 +4,19 @@
 echo "Setting up PostgreSQL database..."
 
 sudo -u postgres psql <<EOF
-DO $$ 
+DO \$\$
 BEGIN 
    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chhaya_demo') THEN
-      CREATE DATABASE chhaya_demo;
+      EXECUTE 'CREATE DATABASE chhaya_demo';
    END IF;
-END $$;
+END \$\$;
 
-DO $$ 
+DO \$\$
 BEGIN 
    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'postgres') THEN
-      CREATE USER postgres WITH PASSWORD 'postgres';
+      EXECUTE 'CREATE USER postgres WITH PASSWORD ''postgres''';
    END IF;
-END $$;
+END \$\$;
 
 GRANT ALL PRIVILEGES ON DATABASE chhaya_demo TO postgres;
 EOF
