89dc1f7cd34832e6b42eaea8d181e25433d99b2c
sanketl0 <sanketlodhe666@gmail.com>
2025-04-24 18:26:32 +0530

    changes in missing person form

--- Frontend/main/src/envirnment/envirnment.ts
+++ Frontend/main/src/envirnment/envirnment.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: false,
-    // apiUrl: 'http://localhost:8000/',
-    apiUrl: 'http://63.250.52.91:8077/',
+    apiUrl: 'http://localhost:8000/',
+    // apiUrl: 'http://63.250.52.91:8090/',
     geo_url : "https://products.coderize.in/geoserver/GeoFlow_WCD/wms/",
-    // person_geoserver_url: 'http://localhost:8080/geoserver/chhaya_demo',
-    person_geoserver_url: 'https://products.coderize.in/geoserver/chhaya_demo',
+    person_geoserver_url: 'http://localhost:8080/geoserver/chhaya_demo',
+    // person_geoserver_url: 'https://products.coderize.in/geoserver/chhaya_demo',
     featureFlag: true
 
   };
