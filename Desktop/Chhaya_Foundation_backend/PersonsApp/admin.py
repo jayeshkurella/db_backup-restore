2b6039f04cb6ed71c1fe8e34d6e697921342927f
jayeshkurella <138563650+jayeshkurella@users.noreply.github.com>
2026-02-12 12:12:30 +0530

    Delete Desktop directory

--- Desktop/Chhaya_Foundation_backend/PersonsApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import MissingPerson
-from leaflet.admin import LeafletGeoAdmin
-
-class MissingPersonAdmin(LeafletGeoAdmin):
-    list_display = ('name', 'last_known_location', 'date_missing')
-
-admin.site.register(MissingPerson, MissingPersonAdmin)
